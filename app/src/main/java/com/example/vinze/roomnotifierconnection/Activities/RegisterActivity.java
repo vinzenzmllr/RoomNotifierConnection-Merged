fd258546f701ffd1f2affd16b5d4be29b9613098
vinze <vinzenz.m5533@gmail.com>
2019-03-26 20:29:17 +0100

    26.03. was eins nices Layout

--- app/src/main/java/com/example/vinze/roomnotifierconnection/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/vinze/roomnotifierconnection/Activities/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.vinze.roomnotifierconnection.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.vinze.roomnotifierconnection.R;
 
@@ -11,5 +12,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void OnClickUserData(View v){
+
     }
 }
