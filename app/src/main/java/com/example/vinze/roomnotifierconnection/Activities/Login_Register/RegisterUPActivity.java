193870e1b089e9ff2e172170f365e30f248b0979
Daniel <danielstb@gmx.at>
2019-04-25 14:37:03 +0200

    registrieren aud db ohne risks

--- app/src/main/java/com/example/vinze/roomnotifierconnection/Activities/Login_Register/RegisterUPActivity.java
+++ app/src/main/java/com/example/vinze/roomnotifierconnection/Activities/Login_Register/RegisterUPActivity.java
@@ -5,13 +5,25 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.vinze.roomnotifierconnection.Entities.User;
+import com.example.vinze.roomnotifierconnection.JSONParser;
 import com.example.vinze.roomnotifierconnection.R;
-import com.example.vinze.roomnotifierconnection.UserProcessor.UserBuilder;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.example.vinze.roomnotifierconnection.R.color.colorAccent;
 
@@ -21,6 +33,14 @@ public class RegisterUPActivity extends AppCompatActivity {
     public EditText et_password;
     public TextView tv_usernameDescription;
     public TextView tv_passwordDescription;
+
+    JSONParser jsonParser = new JSONParser();
+
+    private static final String url_add_user = "http://lucidus.htl5.org/phpshit/create_user.php";
+
+    private static final String TAG_SUCCESS = "success";
+
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +53,6 @@ public class RegisterUPActivity extends AppCompatActivity {
         tv_passwordDescription = findViewById(R.id.tv_passwordDescription);
 
     }
-
 
     private void handleError(int errorFlag){
         if(errorFlag == 1){
@@ -65,10 +84,68 @@ public class RegisterUPActivity extends AppCompatActivity {
         }
         else
         {
-            UserBuilder.buildUser.setUsername(et_username.getText().toString());
-            UserBuilder.buildUser.setPassword(et_password.getText().toString());
+            //UserBuilder.buildUser.setUsername(et_username.getText().toString());
+            //UserBuilder.buildUser.setPassword(et_password.getText().toString());
+
+            new AddNewUser().execute();
+
+            username = et_username.getText().toString();
+
+            System.out.println("************************************************************");
+            if (username == null)
+                System.out.println("Leer | " + et_username.getText().toString());
+            else
+                System.out.println(username);
+            System.out.println("************************************************************");
+
             Intent switchToUserData = new Intent(this,RegisterDataActivity.class);
+            switchToUserData.putExtra("EXTRA_USERNAME", username);
             startActivity(switchToUserData);
+        }
+
+    }
+
+    class AddNewUser extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        protected String doInBackground(String... args) {
+            User user = new User();
+            user.setPasswort(et_password.getText().toString());
+            user.setUsername(et_username.getText().toString());
+
+            List<NameValuePair> params = new ArrayList<>();
+            params.add(new BasicNameValuePair("u_username", user.getUsername()));
+            params.add(new BasicNameValuePair("u_passwort", user.getPasswort()));
+
+            // getting JSON Object
+            // Note that create product url accepts POST method
+            JSONObject json = jsonParser.makeHttpRequest(url_add_user,"POST", params);
+
+            System.out.println(params.toString());
+            System.out.println("Create Response" + json.toString());
+
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    System.out.println("user erstellt");
+                } else {
+                    System.out.println("fehler beim erstellen des users");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(String file_url) {
+
         }
 
     }
